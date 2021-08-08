e80538a76679a169adf63c71aeacde1a92b4de10
yvette <13071251886@163.com>
2021-08-08 19:13:47 +0800

    feat: init eslint

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{ fontSize: 24, marginRight: 12 }}/>
       </header>
     </div>
   );
